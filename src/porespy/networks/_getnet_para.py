047bc02b23e3fe93c582a90b864e9907a7f2d93e
Rômulo Cenci <romulo.cenci@ltrace.com.br>
2024-06-27 10:37:55 -0300

    Fixes big throats after pyedt update

--- src/porespy/networks/_getnet_para.py
+++ src/porespy/networks/_getnet_para.py
@@ -218,7 +218,7 @@ def regions_to_network_parallel(
     if im.size != phases.size:
         raise Exception('regions and phase are different sizes, probably ' +
                         'because boundary regions were not added to phases')
-    dt = edt(phases >= 1, scale=voxel_size)
+    dt = pyedt.edt(phases >= 1, scale=voxel_size, sqrt_result=True)
 
     # Get 'slices' into im for each pore region
     slices = spim.find_objects(im)
