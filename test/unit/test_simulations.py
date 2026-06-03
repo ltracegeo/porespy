4e545251c87869b368d9b26785333bbdf9e9c38c
Robim Pacheco <robim.pacheco@ltrace.com.br>
2026-06-03 14:01:51 -0300

    PL-3186 Replace skimage`s skeletonize_3d with new skeletonize

--- test/unit/test_simulations.py
+++ test/unit/test_simulations.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 import porespy as ps
 import scipy.ndimage as spim
-from skimage.morphology import disk, ball, skeletonize_3d
+from skimage.morphology import disk, ball, skeletonize
 from skimage.util import random_noise
 from scipy.stats import norm
 try:
